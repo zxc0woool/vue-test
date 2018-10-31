8f4826bc8494c25bac3488fd74f0c1d99bc311fd
谢霖 <l>
2018-10-31 09:56:52 +0800

    修复跨域BUG

--- server/app.js
+++ server/app.js
@@ -3,19 +3,22 @@ const express = require('express');
 const app = express();
 
 const routes = require('./routes');
-
+process.env.NODE_ENV = "production";
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+if(process.env.NODE_ENV === "production"){
+  app.use(function(req, res, next){
 
-app.use(function(res){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials','true');
-  next();
-})
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials','true');
+    next();
+  })
+}
+
 
 
 app.get('/api/getArticle', (req, res, next) => {
